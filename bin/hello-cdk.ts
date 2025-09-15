be8c4d02c138d0f90fd761c126e84c3d14fbeda1
k-miyazoe <nk.miyazoe@gmail.com>
2025-09-15 10:11:31 +0900

    cdk Tutorial

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -4,6 +4,7 @@ import { HelloCdkStack } from '../lib/hello-cdk-stack';
 
 const app = new cdk.App();
 new HelloCdkStack(app, 'HelloCdkStack', {
+  env: { account: '211125646845', region: 'ap-northeast-1' },
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
