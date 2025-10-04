c1bbfc2726326119880245c4a61c8419345503e0
k-miyazoe <nk.miyazoe@gmail.com>
2025-10-04 09:55:12 +0900

    add sns subscriptions

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as snsSubscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -20,7 +23,7 @@ export class HelloCdkStack extends cdk.Stack {
       `),
     });
 
-     // Define the Lambda function URL resource
+    // Define the Lambda function URL resource
     const myFunctionUrl = myFunction.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
     });
@@ -30,5 +33,44 @@ export class HelloCdkStack extends cdk.Stack {
       value: myFunctionUrl.url,
     })
 
+    // SNSトピックの作成
+    const topic = new sns.Topic(this, 'NotificationTopic', {
+      displayName: 'Email Notification Topic',
+      topicName: 'email-notification-topic',
+    });
+
+    // Parameter Storeから既存のメールアドレスリストを取得
+    // valueFromLookupは同期的にParameter Storeから値を取得します
+    const emailsParam = ssm.StringParameter.valueFromLookup(
+      this,
+      '/mailaddress/operator'
+    );
+
+    // カンマ区切りの文字列を配列に変換してサブスクリプション作成
+    const emailList = emailsParam.split(',').map(e => e.trim()).filter(e => e);
+
+    emailList.forEach((email, index) => {
+      topic.addSubscription(
+        new snsSubscriptions.EmailSubscription(email)
+      );
+
+      new cdk.CfnOutput(this, `SubscribedEmail${index + 1}`, {
+        value: email,
+        description: `Subscribed email address ${index + 1}`,
+      });
+    });
+
+    // SNSトピックARNの出力
+    new cdk.CfnOutput(this, 'TopicArn', {
+      value: topic.topicArn,
+      description: 'SNS Topic ARN',
+    });
+
+    new cdk.CfnOutput(this, 'EmailParameter', {
+      value: '/mailaddress/operator',
+      description: 'Parameter Store name for email addresses',
+    });
   }
 }
+
+
